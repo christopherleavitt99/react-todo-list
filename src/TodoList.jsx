8eac5eb9a38ec052392217b6ef57e983852593e2
christopherleavitt99 <cml95@byui.edu>
2024-09-21 16:26:33 -0600

    upload to github

---
+++ src/TodoList.jsx
@@ -0,0 +1,14 @@
+import { Todoitem } from "./Todoitem"
+
+export function TodoList( { todos, toggleTodo, deleteTodo }) {
+    return (
+    <ul className="list">
+    {todos.length === 0 && "No ToDos"}
+  {todos.map(todo => {
+    return (
+    <Todoitem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    )
+  })}
+  </ul>
+    )
+}
